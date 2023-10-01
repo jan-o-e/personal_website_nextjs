eb81c6f269254f9e69ce0aba053229a78f69d400
jan-o-e <82890641+jan-o-e@users.noreply.github.com>
2023-10-01 19:05:15 +0100

    disable prettier

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,6 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function AboutPage() {
